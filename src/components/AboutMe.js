398f268b59befae7b904050582f9f00e55f40440
mchhuo <michaelchhuo10@hotmail.com>
2021-01-25 21:00:18 +1100

    changed font

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import './components.css';
+import React from "react";
+import "./components.css";
 
 const AboutMe = () => {
   return (
@@ -11,30 +11,25 @@ const AboutMe = () => {
       </div>
       <div id="details">
         <div id="profile">
-          <h1>About me</h1>
-          I am an all-round developer with a passion for full-stack web development and programming. Hope to use my knowledge of back-end techniques, an interest in creative UI/UX as well as uses of algorithms in programming challenges. I enjoy working in a team and demonstrate a strong work ethic. Having been a tutor and currently a volleyball coach, I have developed great communication and interpersonal skills.
-
+          <h1>About me</h1>I am an all-round developer with a passion for
+          full-stack web development and programming. Hope to use my knowledge
+          of back-end techniques, an interest in creative UI/UX as well as uses
+          of algorithms in programming challenges. I enjoy working in a team and
+          demonstrate a strong work ethic. Having been a tutor and currently a
+          volleyball coach, I have developed great communication and
+          interpersonal skills.
         </div>
         <div>
           <h1>Details</h1>
           <h3>Name:</h3>
-          <div>
-            Michael Chhuo
-          </div>
-          <br />
-          <h3>Age:</h3>
-          <div>
-            22 years
-          </div>
+          <div>Michael Chhuo</div>
           <br />
           <h3>Email:</h3>
-          <div>
-            michaelchhuo10@hotmail.com
-          </div>
+          <div>michaelchhuo10@hotmail.com</div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutMe
+export default AboutMe;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "../src/fonts/Montserrat-Regular.ttf";
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-  );
-document.title = "Michael Chhuo Resume"
+  document.getElementById("root")
+);
+document.title = "Michael Chhuo Resume";
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
